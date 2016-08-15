d1deb7af3641f225b848bb102436812130fd8dad
pulum <pulum@localhost.(none)>
2016-08-15 12:12:41 +0530

    created package

---
+++ src/com/algo/arrays/OddOccurrencesInArray.java
@@ -0,0 +1,32 @@
+package com.algo.arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class OddOccurrencesInArray {
+
+	public static void main(String[] args) {
+		int[] A = { 5, 4, 7, 7, 9, 3, 9, 3, 9, 7, 9, 7, 5 };
+		System.out.println(solution(A));
+
+	}
+
+	public static int solution(int[] A) {
+		int res = 0;
+		Map<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
+		for (int i = 0; i < A.length; i++) {
+			if (map.get(A[i]) == null)
+				map.put(A[i], 1);
+			else {
+				int cnt = map.get(A[i]) + 1;
+				map.put(A[i], cnt);
+			}
+		}
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() % 2 != 0)
+				res = entry.getKey();
+		}
+
+		return res;
+	}
+}
---
+++ src/com/algo/prefixsum/PassingCars.java
@@ -0,0 +1,10 @@
+package com.algo.prefixsum;
+
+public class PassingCars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
